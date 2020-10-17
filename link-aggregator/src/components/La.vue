c3539d079958e917a13b42859aaed05401a1cca7
Delor Rotstrump <delor@delor.pl>
2020-10-17 15:07:38 +0200

    css style changes

--- link-aggregator/src/components/La.vue
+++ link-aggregator/src/components/La.vue
@@ -1,139 +1,151 @@
 <template>
-  <div>
+  <div class="la_main">
     <la-tags-list
       :tags="tags"
       v-on:remove-tag="onRemoveTag($event)"
     ></la-tags-list>
     <la-cards-list
-        :cards="cards"
-        v-on:remove-card="onRemoveCard($event)"
+      :cards="cards"
+      v-on:remove-card="onRemoveCard($event)"
     ></la-cards-list>
   </div>
 </template>
 
 <script>
-import LaCardsList from '@/components/LaCardsList.vue'
-import LaTagsList from '@/components/LaTagsList.vue'
+import LaCardsList from "@/components/LaCardsList.vue";
+import LaTagsList from "@/components/LaTagsList.vue";
 
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 export default {
-   data: function() {
-        return {
-            info: null,
-            loading: true,
-            errored: false,
-            cards: {
-                1: {
-                    links: {
-                        id: 3,
-                        url: ""
-                    }
-                }
-            },
-            tags : {
-              1: {
-                id: 1,
-                name: "TestowyTag",
-                uri: "http_smth_uri"
-              }
-            }
-        }
-    },
+  data: function () {
+    return {
+      info: null,
+      loading: true,
+      errored: false,
+      cards: {
+        1: {
+          links: {
+            id: 3,
+            url: "",
+          },
+        },
+      },
+      tags: {
+        1: {
+          id: 1,
+          name: "TestowyTag",
+          uri: "http_smth_uri",
+        },
+      },
+    };
+  },
   methods: {
     /* Cards */
     apiGetCards: function () {
       var self = this;
-      for(var id in this.cards) {
-        this.cards[id].loading = true
+      for (var id in this.cards) {
+        this.cards[id].loading = true;
       }
       axios
-      .get('/api/cards')
-      .then(response => {
-        self.replaceAllCards(response.data)
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
+        .get("/api/cards")
+        .then((response) => {
+          self.replaceAllCards(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
     },
     apiDeleteCard: function (id) {
-        this.cards[id].loading = true
+      this.cards[id].loading = true;
       axios
-      .delete('/api/cards/'+id)
-      .then(response => {
-        response.done = true
-        this.$delete(this.cards, id)
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading =false)
+        .delete("/api/cards/" + id)
+        .then((response) => {
+          response.done = true;
+          this.$delete(this.cards, id);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
     },
     replaceAllCards: function (cards) {
-        for(var card_id in cards) {
-            var id = cards[card_id].id
-            cards[card_id].loading = false
-            this.$set(this.cards, id, cards[card_id])
-        }
-        // TODO: remove cards when card.loading == true
+      for (var card_id in cards) {
+        var id = cards[card_id].id;
+        cards[card_id].loading = false;
+        this.$set(this.cards, id, cards[card_id]);
+      }
+      // TODO: remove cards when card.loading == true
     },
-    onRemoveCard: function(id) {
-        this.apiDeleteCard(id)
+    onRemoveCard: function (id) {
+      this.apiDeleteCard(id);
     },
     /* Tags */
     apiGetTags: function () {
       var self = this;
-      for(var id in this.tags) {
-        this.tags[id].loading = true
+      for (var id in this.tags) {
+        this.tags[id].loading = true;
       }
       axios
-      .get('/api/tags')
-      .then(response => {
-        self.replaceAllTags(response.data)
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
+        .get("/api/tags")
+        .then((response) => {
+          self.replaceAllTags(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
     },
     apiDeleteTag: function (id) {
-        this.tags[id].loading = true
+      this.tags[id].loading = true;
       axios
-      .delete('/api/tags/'+id)
-      .then(response => {
-        response.done = true
-        this.$delete(this.tags, id)
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading =false)
+        .delete("/api/tags/" + id)
+        .then((response) => {
+          response.done = true;
+          this.$delete(this.tags, id);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
     },
     replaceAllTags: function (tags) {
-        for(var tag_id in tags) {
-            var id = tags[tag_id].id
-            tags[tag_id].loading = false
-            this.$set(this.tags, id, tags[tag_id])
-        }
-        // TODO: remove cards when card.loading == true
+      for (var tag_id in tags) {
+        var id = tags[tag_id].id;
+        tags[tag_id].loading = false;
+        this.$set(this.tags, id, tags[tag_id]);
+      }
+      // TODO: remove cards when card.loading == true
     },
-    onRemoveTag: function(id) {
-        console.log('Removing tag ', id)
-        this.apiDeleteTag(id)
-    }
+    onRemoveTag: function (id) {
+      console.log("Removing tag ", id);
+      this.apiDeleteTag(id);
+    },
   },
-  mounted () {
-    this.apiGetTags()
-    this.apiGetCards()
+  mounted() {
+    this.apiGetTags();
+    this.apiGetCards();
   },
   components: {
-    'la-cards-list': LaCardsList,
-    'la-tags-list': LaTagsList,
+    "la-cards-list": LaCardsList,
+    "la-tags-list": LaTagsList,
   },
 };
 </script>
+<style scoped>
+.la_main {
+  display: -webkit-box;
+  display: -moz-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  flex-direction: row;
+  height: -moz-calc(100% - 64px);
+  height: calc(100% - 64px);
+}
+</style>
--- link-aggregator/src/components/LaTagsList.vue
+++ link-aggregator/src/components/LaTagsList.vue
@@ -25,3 +25,33 @@ export default {
     props: ['tags']
 };
 </script>
+<style scoped>
+body {
+    color: #e8eaed;
+    direction: ltr;
+    font-family: 'Roboto',arial,sans-serif;
+    font-size: 15px;
+}
+body {
+    background-color: #202124;
+    box-sizing: border-box;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    flex-direction: column;
+    height: -moz-calc(100% - 64px);
+    height: calc(100% - 64px);
+    min-height: auto;
+    overflow: hidden;
+    padding-top: 8px;
+    position: fixed;
+    top: 64px;
+    transition-duration: 150ms;
+    transition-property: width,box-shadow,border-radius;
+    transition-timing-function: cubic-bezier(0.4,0.0,0.2,1);
+    width: 280px;
+    z-index: 985;
+}
+</style>
